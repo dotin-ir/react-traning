9a06ae1fae92a5b47b101fc6905a31afbe08adcd
e.khodadadi <khodadady.ehsan@gmail.com>
2017-11-17 20:44:21 +0330

    Prepare for training!

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,31 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import State from "./component/State.jsx";
-import City from "./component/City.jsx";
 
 class App extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            stateKey: "1"
-        };
-        this.handleStateChanged = this.handleStateChanged.bind(this);
-    }
-
-    handleStateChanged(stateKey) {
-        console.log(stateKey);
-        this.setState({stateKey: stateKey});
-    }
-
     render () {
-        return (
-            <div>
-                <State onChange={this.handleStateChanged}/>
-                <City stateKey={this.state.stateKey}/>
-            </div>
-        );
+        return <p> Hello React!</p>;
     }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
